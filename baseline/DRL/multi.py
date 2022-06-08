d9536240bad385dba9887593b6f84bb3bffcfa37
Lennart Kerkvliet <lennart.kerkvliet@me.com>
2022-06-08 12:59:45 +0200

    Fix bug

--- baseline/DRL/multi.py
+++ baseline/DRL/multi.py
@@ -9,7 +9,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class fastenv():
     def __init__(self, 
                  max_episode_length=10, env_batch=64, \
-                 input_dir, writer=None):
+                 input_dir="", writer=None):
         self.max_episode_length = max_episode_length
         self.env_batch = env_batch
         self.env = Paint(self.env_batch, self.max_episode_length)
